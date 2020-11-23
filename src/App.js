87e3b402a8177c38fa8cfeb581b4b8f2d82d895e
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-22 21:12:48 -0500

    g

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
+import { useState, useEffect } from 'react';
+import { MenuItem, FormControl, Select } from '@material-ui/core';
+import InfoBox from './InfoBox';
+import Map from './Map';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState(['USA', 'UK', 'PERU', 'ITALY']);
+  const [country, setCountry] = useState('Worldwide');
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+
+          <FormControl className="app_dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="Worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={123} total={2000} />
+          <InfoBox title="Recovered" cases={123} total={3000} />
+          <InfoBox title="Deaths" cases={123} total={4000} />
+        </div>
+        <Map />
+      </div>
+
+      <div className="app__right">
+        {/* <Table /> */}
+        {/* <Graph /> */}
+      </div>
     </div>
   );
 }
